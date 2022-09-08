8b03035cffbc9abd8961951f299ee8010c9e79f1
Amagi_Yukisaki <xingzeyu@sjtu.edu.cn>
2022-09-09 03:07:07 +0800

    chore: use iPhone 14 Pro Max UA

--- constraints.py
+++ constraints.py
@@ -1,6 +1,6 @@
 REQUESTS_COOKIE_DEFAULT = {}
 
-REQUESTS_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148; TaskCenterApp/3.2.6/iPhone 13  Pro Max/ScreenFringe'
+REQUESTS_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148; TaskCenterApp/3.2.6/iPhone 14  Pro Max/ScreenFringe'
 
 REQUESTS_REFER_BASEURL = 'https://door.sjtu.edu.cn/ui?roomid={}'
 
