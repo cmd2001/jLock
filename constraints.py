1a88fc6a59c1e57db818dccdec72d43b392f0603
Amagi_Yukisaki <xingzeyu@sjtu.edu.cn>
2022-09-08 21:38:33 +0800

    chore: purge google analytics cookies

--- constraints.py
+++ constraints.py
@@ -1,9 +1,4 @@
-REQUESTS_COOKIE_DEFAULT = {
-  '_ga': 'GA1.3.1417690954.1634542459',
-  '_gat': '1',
-  '_gat_gtag_UA_93968395_3': '1',
-  '_gid': 'GA1.3.318251952.1651302553'
-}
+REQUESTS_COOKIE_DEFAULT = {}
 
 REQUESTS_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148; TaskCenterApp/3.2.6/iPhone 13  Pro Max/ScreenFringe'
 
