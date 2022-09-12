8882f1799dfe3db476d6cf23def8b73668e51124
Amagi_Yukisaki <xingzeyu@sjtu.edu.cn>
2022-09-12 21:16:23 +0800

    feat: automaton scripts

---
+++ scripts/status.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /Users/Amagi/Desktop/jLock # your project directory
+source ./venv/bin/activate # venv
+
+unset HTTP_PROXY HTTPS_PROXY ALL_PROXY http_proxy https_proxy all_proxy # unset proxies 
+python3 src/entrypoints/status.py
