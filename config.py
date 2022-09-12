1b55449c9de9b59ca696921f5f739e9fc00caff0
Amagi_Yukisaki <xingzeyu@sjtu.edu.cn>
2022-09-12 21:02:04 +0800

    chore: rm deprecated files

--- config.py
+++
@@ -1,8 +0,0 @@
-JACCOUNT_USERNAME = 'Amagi_Yukisaki' # Your jAccount username here, for example '[username]@sjtu.edu.cn'
-JACCOUNT_PASSWORD = 'PASSW0RD' # your jAccount password here
-ROOM_ID = '00112233445566778899aabbccddeeff' # 32 hexadecimal bits, scan the QR code on your door, copy the url, then you will get it
-
-REFRESH_SESSION_PERIOD = int(60 * 60 * 1e9)
-RELOCK_PERIOD = int(5 * 1e9)
-
-LOG_LEVEL = 'INFO'
